44d763472581edb32d0fa093de6cb2cca887b0e9
Kai-Hsin Liu <kaihsyn@gmail.com>
2013-03-28 01:54:59 -0400

    create main page

---
+++ home.py
@@ -0,0 +1,15 @@
+import webapp2
+import jinja2
+import os
+
+class MainHandler(webapp2.RequestHandler):
+	def get(self):
+
+		template = jinja_environment.get_template('views/home.html')
+		self.response.out.write(template.render())
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler)
+], debug=True)
+
+jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
