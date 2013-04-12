0afee12fe00bffe05dadb8ca6a607f8ceb34912f
Kai-Hsin Liu <kaihsyn@gmail.com>
2013-04-12 01:24:32 -0400

    revise code

--- webhook.py
+++ webhook.py
@@ -7,35 +7,36 @@ from secrets import EN_WEBHOOK
 class EvernoteWebhookHDL(webapp2.RequestHandler):
     def get(self):
     	miss_var = False
+        params = {}
 
     	if self.request.get('userId') and self.request.get('userId') != '':
-    		user_id = self.request.get('userId')
+    		params['user_id'] = self.request.get('userId')
     	else:
-    		user_id = '[no userId]'
+    		params['user_id'] = '[no userId]'
     		miss_var = True
 
     	if self.request.get('guid') and self.request.get('guid') != '':
-    		guid = self.request.get('guid')
+    		params['guid'] = self.request.get('guid')
     	else:
-    		guid = '[no guid]'
+    		params['guid'] = '[no guid]'
     		miss_var = True
 
     	if self.request.get('reason'):
     		if self.request.get('reason') == 'create' or self.request.get('reason') == 'update':
-    			reason = self.request.get('reason')
+    			params['reason'] = self.request.get('reason')
     		else:
-    			reason = '[wrong format reason: %s]' % self.request.get('reason')
+    			params['reason'] = '[wrong format reason: %s]' % self.request.get('reason')
 	    		miss_var = True
     	else:
-    		reason = '[no reason]'
+    		params['reason'] = '[no reason]'
     		miss_var = True
 
     	if miss_var:
-    		logging.error('Evernote Webhook: %s %s %s' % (user_id, guid, reason))
+    		logging.error('Evernote Webhook: %s %s %s' % (params['user_id'], params['guid'], params['reason']))
     	else:
-    		logging.info('Evernote Webhook: %s %s %s' % (user_id, guid, reason))
+    		logging.info('Evernote Webhook: %s %s %s' % (params['user_id'], params['guid'], params['reason']))
 
-    	#taskqueue.add(queue_name='sync-evernote', url='/sync/evernote', params={'key': 'abcdgogady'})
+    	taskqueue.add(queue_name='sync-evernote', url='/sync/evernote/note', params=params, method='GET')
 
 app = webapp2.WSGIApplication([
     webapp2.Route('/hook/%s'%EN_WEBHOOK, handler='webhook.EvernoteWebhookHDL:get', name='evernote-webhook', methods=['GET'])
