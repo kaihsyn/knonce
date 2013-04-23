f59acc0cd248704ad95a52bef0b465d768f03d40
Kai-Hsin Liu <kaihsyn@gmail.com>
2013-04-23 03:13:53 -0400

    temporary styles

---
+++ post.py
@@ -0,0 +1,23 @@
+import sys
+if 'lib' not in sys.path:
+	sys.path[0:0] = ['lib']
+
+import logging
+import webapp2
+from webapp2_extras import routes
+
+import request
+from secrets import HOST
+
+class PostHDL(request.RequestHandler):
+	def get(self):
+		self.render('unit/post.html')
+
+app = webapp2.WSGIApplication([
+	routes.DomainRoute('<alias><:(?i)(\.knonce\.com)', [
+	    webapp2.Route('/post', handler='post.PostHDL:get', name='unit-post', methods=['GET'])
+	]),
+	routes.DomainRoute('localhost', [
+	    webapp2.Route('/post', handler='post.PostHDL:get', name='unit-post', methods=['GET'])
+	])
+], debug=True, config=request.app_config)
