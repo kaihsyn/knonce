f3ee179a06d40a3446394d21a8449baf5c5d747f
Kai-Hsin Liu <kaihsyn@gmail.com>
2013-04-26 20:38:24 -0400

    home page display notes

--- knonce/note.py
+++ knonce/note.py
@@ -3,14 +3,14 @@ from google.appengine.ext import ndb
 class Note(ndb.Model):
 	usn = ndb.IntegerProperty(indexed=False)
 	short = ndb.StringProperty(required=True)
-	title = ndb.StringProperty(default='', indexed=False)
-	summary = ndb.TextProperty(default='')
+	title = ndb.StringProperty(default='')
+	summary = ndb.StringProperty(default='')
 	content = ndb.TextProperty(default='')
 	updated = ndb.IntegerProperty(indexed=False)
-	created = ndb.IntegerProperty()
+	created = ndb.IntegerProperty(indexed=False)
 	tag = ndb.StringProperty(repeated=True)
 
-	first_add = ndb.DateTimeProperty(auto_now_add=True)
+	date = ndb.DateTimeProperty(auto_now_add=True)
 
 class Tag(ndb.Model):
 	guid = ndb.StringProperty()
--- unit_home.py
+++ unit_home.py
@@ -4,11 +4,36 @@ if 'lib' not in sys.path:
 
 import logging
 import request
-
+import datetime
 from secrets import HOST
+from knonce.note import Note
+from knonce.unit import Unit
+from collections import OrderedDict
 
 class MainHDL(request.RequestHandler):
 	def get(self, alias=None):
-		pageval = { 'host': 'www.%s'%HOST }
+
+		if alias is None:
+			return self.redirect('http://www.knonce.com/')
+
+		""" get unit """
+		unit = Unit.query(Unit.alias==alias).get()
+		if unit is None:
+			return self.redirect('http://www.knonce.com/')
+
+		""" grab latest ten notes """
+		notes, next_curs, more = Note.query(ancestor=unit.key).order(Note.first_add).fetch_page(10, projection=['date', 'short', 'title', 'summary'])
+
+		pageval = {
+			'unit': unit
+			'host': 'www.%s'%HOST,
+			'cursor': next_curs.urlsafe(),
+			'notes': OrderedDict() }
+
+		for note in notes:
+			if note.date not in pageval['notes']:
+				pageval['notes'][note.date] = []
+			pageval['notes'][note.date].append(note)
+
 		self.render('unit/home.html', pageval)
 
