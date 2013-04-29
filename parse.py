e1d4ee145a7187f623a05531c59f25ecbf9e0791
jhihguan <imdiedie@gmail.com>
2013-04-29 13:36:53 +0800

    make parse_ennote return list of result by every en-note tag

--- parse.py
+++ parse.py
@@ -1,27 +1,16 @@
 import re
 
+# parse tag en-note by ' ="
+# return list of all en-note attribute
 def parse_ennote(self):
     matchObj2 = re.findall(r'<en-note(.*?)>',self, re.M|re.I)
-    print "======================"
+    result_list = []
     for match in matchObj2:
-        split = re.split('[ ="]',match)
-        count = 0
+        split = re.split('[\' ="]',match)
+        temp_list = []
         for s in split:
             if s != "":
-                if count % 2 ==0:
-                    print s
-                else :
-                    print "    :",s
-                count+=1
-        print "======================"
+                temp_list.append(s)
+        result_list.append(temp_list)
+    return result_list
 
-
-
-testsplit = """<en-note title="ggg" hey="abdd" bgcolor="red" text="blue">
-hey this is a test <div align="left">ohhhhhh no~~~~~</div><span color="black">good bye gg</span></en-note>
-<en-note title="bbbb" lang="abcde">
-<div>oh my gosh~~~~~</div>
-<span>I am Handsome guy</div></en-note>
-"""
-
-parse_ennote(testsplit)
